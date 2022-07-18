c29005bceb960b7f535a44b93cf9b16bc796c008
CaraErskine <caraerskine@gmail.com>
2022-07-18 12:15:41 -0400

    commit tree 372b50f20309acd0d3e9e7ae54d5ddda24be601b

---
+++ index.js
@@ -0,0 +1,13 @@
+
+myEach([1, 2, 3, 4, 5], alert);
+
+console.log(myEach);
+
+// const originalArray = [1, 2, 3, 4, 5];
+
+// const newArray = originalArray.map(function addOne(number) {
+//   return number + 1;
+// });
+
+// console.log(originalArray); // [1, 2, 3, 4, 5]
+// console.log(newArray); // [2, 3, 4, 5, 6]
